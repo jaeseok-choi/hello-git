e54145c512d45134976db2280ddf808bc1fb37b9
jschoi <jssjchl@hansung.ac.kr>
2020-11-19 23:38:44 +0900

    Fourth Commit

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
 	public static void main(String[] args) {
 		System.out.println("Hello, world form GIT");
 		System.out.println("Change after First commit");
-	
+	    System.out.println("Change after Pushing to remote!");
 
     }
 }
